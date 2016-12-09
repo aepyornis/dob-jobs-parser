5cb0643fe54c1392f16e24df1c4021fd2d10ab81
ziggy <zig@riseup.net>
2016-12-09 09:46:37 -0500

    Use pluto to get lat & lng instead of lookup table

--- sql/geocode.sql
+++ sql/geocode.sql
@@ -1,38 +1,24 @@
--- this adds a bbl_lookup table
+-- This assumes that there is a table pluto16v2 in the database
+-- that has lat and lng columns
 BEGIN;
 
 -- index bbl
 create index on dobjobs (bbl);
 
-
-DROP TABLE IF EXISTS bbl_lookup;
-
-CREATE TABLE bbl_lookup (
-       lat numeric,
-       lng numeric,
-       bbl text primary key
-);
-
-COPY bbl_lookup(lat, lng, bbl) from '/home/michael/data/pluto/bbl_lat_lng.txt' CSV HEADER;
-
--- this updates the jobs table with lat/lng from the bbl table.
 alter table dobjobs add column lat_coord numeric;
 alter table dobjobs add column lng_coord numeric;
 
 update dobjobs
-       set lat_coord = bbl_lookup.lat,
-       lng_coord = bbl_lookup.lng     
+       set lat_coord = pluto16v2.lat,
+       lng_coord = pluto16v2.lng
 from
-   bbl_lookup
+   pluto16v2
 where
-   bbl_lookup.bbl = dobjobs.bbl;
-
+   pluto16v2.bbl = dobjobs.bbl;
 
 COMMIT;
 
 -- multi-column index
 create index on dobjobs (lat_coord, lng_coord);
--- lat
 create index on dobjobs (lat_coord);
--- lng
 create index on dobjobs (lng_coord);
